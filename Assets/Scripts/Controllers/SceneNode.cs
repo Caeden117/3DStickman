94be43dcb3c9a7d9008cd6fcea96915954ee2647
Caeden117 <caeden.s@outlook.com>
2025-11-18 20:14:56 -0800

    SceneNode refactor

--- Assets/Scripts/Controllers/SceneNode.cs
+++ Assets/Scripts/Controllers/SceneNode.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 // Code largely taken from the example source code on GitHub, but I've made my own modifications
@@ -6,7 +7,7 @@ using System.Collections.Generic;
 public class SceneNode : MonoBehaviour {
 
     protected Matrix4x4 CombinedTransform;
-    
+
     public Vector3 NodeOrigin = Vector3.zero;
 
     private readonly List<NodePrimitive> primitiveList = new();
@@ -21,12 +22,7 @@ public class SceneNode : MonoBehaviour {
         // determine if this is a root node
         isRoot = transform.parent == null || !GetComponentInChildren<SceneNode>();
 
-        // gather primitives attached to this node
-        foreach (var primitive in GetComponents<NodePrimitive>())
-        {
-            primitiveList.Add(primitive);
-        }
-
+        // gather children and primitives
         foreach (Transform child in transform)
         {
             // gather scene nodes from children and assign them as such
@@ -40,12 +36,29 @@ public class SceneNode : MonoBehaviour {
             var childPrimitives = child.GetComponents<NodePrimitive>();
             if (childPrimitives.Length > 0)
             {
-                foreach (var primitive in childPrimitives)
-                {
-                    primitiveList.Add(primitive);
-                }
+                primitiveList.AddRange(childPrimitives);
             }
         }
+    }
+
+    /// <summary>
+    /// Clears the SceneNode by deleting all child SceneNodes and NodePrimitives.
+    /// This operation is only valid on the root SceneNode.
+    /// </summary>
+    public void ClearChildren()
+    {
+        if (!isRoot)
+        {
+            throw new InvalidOperationException($"{nameof(ClearChildren)} can only be called on a root SceneNode.");
+        }
+
+        foreach (var child in childrenList)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+
+        childrenList.Clear();
+        primitiveList.Clear();
     }
 
     // start the composite process from the root node
@@ -55,12 +68,12 @@ public class SceneNode : MonoBehaviour {
         if (isRoot)
         {
             var identity = Matrix4x4.identity;
-            CompositeXform(ref identity);
+            CompositeTransform(ref identity);
         }
     }
 
-    // This must be called _BEFORE_ each draw!!  @
-    public void CompositeXform(ref Matrix4x4 parentXform)
+    // Computes the transform matrix for this node, then propagates this matrix to all children and primitives.
+    protected void CompositeTransform(ref Matrix4x4 parentXform)
     {
         var orgT = Matrix4x4.Translate(NodeOrigin);
         var trs = Matrix4x4.TRS(transform.localPosition, transform.localRotation, transform.localScale);
@@ -69,7 +82,7 @@ public class SceneNode : MonoBehaviour {
 
         // propagate to all children
         foreach (var child in childrenList)
-            child.CompositeXform(ref CombinedTransform);
+            child.CompositeTransform(ref CombinedTransform);
         
         // disseminate to primitives
         foreach (var primitive in primitiveList)
